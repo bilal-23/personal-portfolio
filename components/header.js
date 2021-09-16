9837228719217d740303658f3d5a083fff09fc2a
Bilal <mansuribilal101@gmail.com>
2021-09-16 20:21:29 +0530

    🚀 Added: Header Section

--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ function Header() {
     return (
         <header className={classes.header}>
             <h1>Hello! <span className={classes.emoji_wave}>👋</span></h1>
-            <h2 className={classes.header_info}>I'm Md Bilal Mansuri, a front-end software developer focused on building beautiful web-apps.</h2>
+            <h2 className={classes.header_info}>I'm Md Bilal Mansuri, a front-end developer focused on building beautiful web-apps.</h2>
             <h3 className={classes.header_contact}> <span>Get in touch </span> <span className={classes.emoji_pointer}>👉</span><span> <a href="mailto:mansuribilal101@gmail.com" target="_blank" className={classes.header_contact_link}>mansuribilal101@gmail.com</a> </span> </h3>
         </header>
     )
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charset="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1,shrink-to-fit=no"
+        />
+        <meta name="Bilal Mansuri" content="Frontend developer, React Developer, Javascript Developer" />
+        <meta name="Md Bilal Mansuri" content="Frontend developer, React Developer, Javascript Developer" />
+        <meta name="Bilal" content="portfolio, javascript, developer" />
+        <title>Md Bilal Mansuri</title>
+      </Head>
+      <Component {...pageProps} />
+    </>)
 }
 
 export default MyApp
