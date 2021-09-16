1935233f49eed9476bf945f533219c3a5e8f4c05
Bilal <mansuribilal101@gmail.com>
2021-09-16 21:58:26 +0530

    ✨ Chore: Quotation Fix

--- components/background.js
+++ components/background.js
@@ -9,7 +9,7 @@ function Background() {
             <div className={classes.background_content}>
                 <p>I&rsquo;m currently pursuing Engineering in Electronics and Communication at <a href="https://www.medicaps.ac.in/" className={classes.background_link} target="_blank" rel="noreferrer">Medi-Caps University.</a> I&rsquo;m also a web development mentor at <a href="https://gdsc.community.dev/medi-caps-university-indore/" className={classes.background_link} target="_blank" rel="noreferrer">Google Developer Student Club</a> on my University Campus.</p>
                 <p>As a frontend developer, I enjoy learning new frameworks and libraries. Recently I have studied Next.js which is a react framework. When developing web-apps I aim for a beautiful, responsive product with pixel-perfect user experience.</p>
-                <p><span>When I'm not coding</span>, I'm probably reading a book, or playing online games.</p>
+                <p><span>When I&apos;m not coding</span>, I'm probably reading a book, or playing online games.</p>
             </div>
         </Section>
     )
