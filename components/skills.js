2e777970cd5ad8a84fa969247c5277df78190a01
Bilal <mansuribilal101@gmail.com>
2021-09-17 19:42:57 +0530

    💡 Feat: Skills section

---
+++ components/skills.js
@@ -0,0 +1,47 @@
+import classes from './skills.module.css';
+import Section from './section';
+function Skills() {
+    return (
+        <Section>
+            <div className="section_title">
+                SKILLS
+            </div>
+            <div className="section_content">
+                <div className={classes.skills}>
+                    <div className={classes.skills_category}>
+                        <div className={classes.skills_category}>LANGUAGES</div>
+                        <ul>
+                            <li className={classes.skills_category_item}>Javascript&#40;ES6&#41;</li>
+                            <li className={classes.skills_category_item}>CSS/SCSS</li>
+                            <li className={classes.skills_category_item}>HTML</li>
+                            <li className={classes.skills_category_item}>Java</li>
+                            <li className={classes.skills_category_item}>C</li>
+                        </ul>
+                    </div>
+                    <div className={classes.skills_category}>
+                        <div className={classes.skills_category}>TOOLS</div>
+                        <ul>
+                            <li className={classes.skills_category_item}>Git &#38; Github</li>
+                            <li className={classes.skills_category_item}>MongoDB</li>
+                            <li className={classes.skills_category_item}>Firebase</li>
+                            <li className={classes.skills_category_item}>Chrome DevTools
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={classes.skills_category}>
+                        <div className={classes.skills_category}>FRAMEWORKS/LIBRARIES</div>
+                        <ul>
+                            <li className={classes.skills_category_item}>Next.js</li>
+                            <li className={classes.skills_category_item}>React</li>
+                            <li className={classes.skills_category_item}>Redux</li>
+                            <li className={classes.skills_category_item}>React-Router</li>
+                            <li className={classes.skills_category_item}>Material-UI</li>
+                            <li className={classes.skills_category_item}>Bootstrap</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </Section>
+    )
+}
+export default Skills;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Background from "../components/background";
 import Header from "../components/header";
+import Skills from "../components/skills";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Background />
+      <Skills />
     </>
   )
 }
