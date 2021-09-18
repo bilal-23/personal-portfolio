fcad0df86c805c67b25fffdc67954c5717f6bcaa
Bilal <mansuribilal101@gmail.com>
2021-09-18 13:14:09 +0530

    ✨ Chore: Remove forbidden script

--- components/social-link.js
+++ components/social-link.js
@@ -1,13 +1,9 @@
 import classes from './social-link.module.css';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import Head from 'next/head';
 
 function SocialLinks(props) {
     return (
         <>
-            <Head>
-                <script src="https://kit.fontawesome.com/d3c372b43e.js" crossorigin="anonymous"></script>
-            </Head>
             <div className={classes.box}>
                 <a href={props.link} target="_blank" rel="noopener noreferrer">
                     {props.image !== 'gmail' && < img src={`/images/${props.image}.svg`} alt="github" className={`${props.image === 'stackoverflow' && classes.stackoverflow}`} />}
