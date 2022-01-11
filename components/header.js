d416083a672ea45ad282ef70c2e3bd74720381a0
Bilal <mansuribilal101@gmail.com>
2022-01-11 19:43:27 +0530

    updated resume

--- components/header.js
+++ components/header.js
@@ -11,7 +11,7 @@ function Header() {
                 <h3 className={classes.header_tag}>front-end developer</h3>
                 <h4 className={classes.header_details}>Front-end developer focused on building webapps with pixel perfect User Experince.</h4>
                 <div className={classes.cta}>
-                    <Button> <a lass="resume" href="./Md-Bilal-Mansuri.pdf" target="_blank" rel="noopener noreferrer">Downlod Resume</a> </Button>
+                    <Button> <a className="resume" href="https://drive.google.com/file/d/1Q6tEXcCoDRuvu7YCt0CrlB7AxAEwCmZ4/view?usp=sharing" target="_blank" rel="noopener noreferrer">Downlod Resume</a> </Button>
                 </div>
                 <h3 className={classes.header_contact}> <span>Get in touch </span> <span className={classes.emoji_pointer}>👉</span><span> <a href="mailto:mansuribilal101@gmail.com" target="_blank" rel="noopener noreferrer" className={classes.header_contact_link}>mansuribilal101@gmail.com</a> </span> </h3>
             </header>
