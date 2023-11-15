7d33a0c78221451272f83a8fbf7d1da367ab674e
Bilal <mansuribilal101@gmail.com>
2023-11-15 22:19:55 +0530

    :zap: Fixed error

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,7 @@ function MyApp({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1,shrink-to-fit=no"
         />
+        <link rel="shortcut icon" href="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20100%20100%22%3E%3Ctext%20y%3D%22.9em%22%20font-size%3D%2290%22%3E%F0%9F%91%A8%E2%80%8D%F0%9F%92%BB%3C%2Ftext%3E%3C%2Fsvg%3E" type="image/svg+xml"></link>
         <meta name="title" content="Md Bilal Mansuri, Frontend developer, React Developer, Javascript Developer" />
         <meta name="description" content="Frontend developer, React Developer, Javascript Developer" />
         <title>Bilal Mansuri</title>
